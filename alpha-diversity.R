7618bd5432edec691d2999a904c17e74b8f7d3eb
L-Coates <75804642+L-Coates@users.noreply.github.com>
2024-12-15 16:02:40 -0800

    Add files via upload

--- alpha-diversity.R
+++ alpha-diversity.R
@@ -105,6 +105,7 @@ print(observed_features.md1)
 library(bestNormalize)
 bestNormalize(metadata.v3$observed_features)
 metadata.v3$observed_features.boxcox.transformed <- boxcox(metadata.v3$observed_features, standardize=T)$x.t
+
 set.seed(123)
 observed_features.md1<-summary(lmer(observed_features.boxcox.transformed~stool_age_days+infant.antibiotic+ infant.probiotic+infant.laxative+infant.gender + mode.of.delivery+first.pregnancy+household.size+ExclusiveBreastfeeding.before.4months + infant.diarrhea+infant.fever+infant.vomit+(1|mid), data=metadata.v3))
 print(observed_features.md1)
@@ -158,6 +159,36 @@ age.vs.evenness <-ggplot(aes(x=stool_age_days, y=pielou_evenness, group=mid), da
 ggarrange(age.vs.shannon, age.vs.faith, age.vs.observedASV,age.vs.evenness, ncol=2, nrow=2, labels="AUTO")
 ggsave("age.vs.alphadiversity.jpeg", plot=last_plot(), height=10, width=13, dpi=600)
 
+#try another figure for age vs. alpha diversity that includes the regression line
+#and confidence interval. 
+shannon.full.model <-lmer(shannon_entropy~stool_age_days+infant.antibiotic+ infant.probiotic+infant.laxative+infant.gender + mode.of.delivery+first.pregnancy+household.size+ExclusiveBreastfeeding.before.4months + infant.diarrhea+infant.fever+infant.vomit+(1|mid), data=metadata.v3)
+faith.full.model <- lmer(faith.diversity~stool_age_days+infant.antibiotic+ infant.probiotic+infant.laxative+infant.gender + mode.of.delivery+first.pregnancy+household.size+ExclusiveBreastfeeding.before.4months + infant.diarrhea+infant.fever+infant.vomit+(1|mid), data=metadata.v3)
+observed_features.full.model <- lmer(observed_features.boxcox.transformed~stool_age_days+infant.antibiotic+ infant.probiotic+infant.laxative+infant.gender + mode.of.delivery+first.pregnancy+household.size+ExclusiveBreastfeeding.before.4months + infant.diarrhea+infant.fever+infant.vomit+(1|mid), data=metadata.v3)
+evenness.full.model <- lmer(pielou_evenness~stool_age_days+infant.antibiotic+ infant.probiotic+infant.laxative+infant.gender + mode.of.delivery+first.pregnancy+household.size+ExclusiveBreastfeeding.before.4months + infant.diarrhea+infant.fever+infant.vomit+(1|mid), data=metadata.v3)
+
+age.vs.shannon.RegLine <- predict_response(shannon.full.model, terms = "stool_age_days", ci_level = 0.95) |> plot(show_data=T, use_theme=T, show_title=F)+
+    theme_bw()+
+    xlab("")+
+    ylab("Shannon entropy")
+
+age.vs.faith.RegLine <- predict_response(faith.full.model, terms = "stool_age_days", ci_level = 0.95) |> plot(show_data=T, use_theme=T, show_title=F)+
+    theme_bw()+
+    xlab("")+
+    ylab("Faith's phylogenetic diversity")
+
+age.vs.observedOTU.RegLine <- predict_response(observed_features.full.model, terms = "stool_age_days", ci_level = 0.95) |> plot(show_data=T, use_theme=T, show_title=F)+
+    theme_bw()+
+    xlab("")+
+    ylab("Observed ASVs, Box Cox transformed")
+
+age.vs.evenness.RegLine <- predict_response(evenness.full.model, terms = "stool_age_days", ci_level = 0.95) |> plot(show_data=T, use_theme=T, show_title=F)+
+    theme_bw()+
+    xlab("")+
+    ylab("Pielou's evenness")
+
+
+
+
 table(metadata.v3$infant.antibiotic) #10 stool samples were collected during a visit when an infant had antibiotic.
 antibiotics.vs.shannon<- ggplot(aes(x=infant.antibiotic, y=shannon_entropy), data=metadata.v3)+geom_boxplot()+xlab("antibiotic use")+ ylab("Shannon entropy")+scale_x_discrete(labels=c("no\nn=317", "yes\nn=10"))
 antibiotics.vs.observedASV <- ggplot(aes(x=infant.antibiotic, y=observed_features.boxcox.transformed), data=metadata.v3)+geom_boxplot()+xlab("antibiotic use") + ylab("observed ASVs, Box Cox transformed")+scale_x_discrete(labels=c("no\nn=317", "yes\nn=10")) 
@@ -172,11 +203,35 @@ ggsave("infant.gender.breastfeeding.vs.faithPD.jpeg", plot=last_plot(), dpi=600,
 ggplot(aes(x=household.size, y=pielou_evenness), data=metadata.v3)+geom_boxplot()+xlab("household size")+ ylab("Pielou's evenness")+scale_x_discrete(labels=c("<= 3\nn=72", "> 3\nn=37"))
 ggsave("household.size.vs.evenness.jpeg", plot=last_plot(), dpi=600, height=4, width=4)
 
-diarrhea.vs.shannon <-ggplot(aes(x=infant.diarrhea, y=shannon_entropy, color=as.factor(infant.diarrhea)), data=metadata.v3)+geom_boxplot()+xlab("diarrhea")+ylab("Shannon entropy") +scale_x_discrete(labels=c("no\nn=287", "yes\nn=40"))+scale_color_manual(values=c(no="black", yes="#F8766D"))+theme(legend.position="none")
-diarrhea.vs.observedASV <-ggplot(aes(x=infant.diarrhea, y=observed_features.boxcox.transformed, color=as.factor(infant.diarrhea)), data=metadata.v3)+geom_boxplot()+xlab("diarrhea")+ylab("observed ASVs, Box Cox transformed") +scale_x_discrete(labels=c("no\nn=287", "yes\nn=40")) +scale_color_manual(values=c(no="black", yes="#F8766D"))+theme(legend.position="none")
-diarrhea.vs.evenness <-ggplot(aes(x=infant.diarrhea, y=pielou_evenness, color=as.factor(infant.diarrhea)), data=metadata.v3)+geom_boxplot()+xlab("diarrhea")+ylab("Peilou's evenness")+scale_x_discrete(labels=c("no\nn=287", "yes\nn=40"))+scale_color_manual(values=c(no="black", yes="#F8766D"))+theme(legend.position="none")
+diarrhea.vs.shannon <-ggplot(aes(x=infant.diarrhea, y=shannon_entropy, color=as.factor(infant.diarrhea)), data=metadata.v3)+
+    geom_boxplot()+
+    theme_bw()+
+    xlab("diarrhea")+
+    ylab("Shannon entropy") +
+    scale_x_discrete(labels=c("no\nn=287", "yes\nn=40"))+
+    scale_color_manual(values=c(no="black", yes="#F8766D"))+
+    theme(legend.position="none")
+
+diarrhea.vs.observedASV <-ggplot(aes(x=infant.diarrhea, y=observed_features.boxcox.transformed, color=as.factor(infant.diarrhea)), data=metadata.v3)+
+    geom_boxplot()+
+    theme_bw()+
+    xlab("diarrhea")+
+    ylab("observed ASVs, Box Cox transformed") +
+    scale_x_discrete(labels=c("no\nn=287", "yes\nn=40")) +
+    scale_color_manual(values=c(no="black", yes="#F8766D"))+
+    theme(legend.position="none")
+
+diarrhea.vs.evenness <-ggplot(aes(x=infant.diarrhea, y=pielou_evenness, color=as.factor(infant.diarrhea)), data=metadata.v3)+
+    geom_boxplot()+
+    theme_bw()+
+    xlab("diarrhea")+
+    ylab("Peilou's evenness")+
+    scale_x_discrete(labels=c("no\nn=287", "yes\nn=40"))+
+    scale_color_manual(values=c(no="black", yes="#F8766D"))+
+    theme(legend.position="none")
+
 ggarrange(diarrhea.vs.shannon, diarrhea.vs.evenness,diarrhea.vs.observedASV, ncol=3, nrow=1, labels="AUTO")
-ggsave("diarrhea.vs.alphadiversity.jpeg", plot=last_plot(), height=5, width=10, dpi=600)
+ggsave("diarrhea.vs.alphadiversity.tiff", device="tiff", plot=last_plot(), height=5, width=10, dpi=600)
 
 
 
